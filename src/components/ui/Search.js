eaabab04c3c47edf66a119be24c26658e536b3a6
Dmytro Andriushchenko <46615932+Charon89@users.noreply.github.com>
2020-07-08 09:17:03 -0400

    Final push

---
+++ src/components/ui/Search.js
@@ -0,0 +1,13 @@
+import React,{useState} from 'react';
+
+const Searcha = () => {
+    return (
+        <section className='search'>
+            <form action="">
+                <input type="text" className='form-control' placeholder='Search characters' autoFocus/>
+            </form>
+        </section>
+    );
+};
+
+export default Searcha;
